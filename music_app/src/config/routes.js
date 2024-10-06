5666e414d963814d37225826300972510f4a768a
doanhdai <doanhdaigr5.2004@gmail.com>
2024-10-06 12:29:09 +0700

    fix conflict

--- music_app/src/config/routes.js
+++ music_app/src/config/routes.js
@@ -1,5 +1,4 @@
 const routes = {
-<<<<<<< HEAD
   Home: "/",
   Artist: "/artist",
   Albums: "/albums",
@@ -13,20 +12,9 @@ const routes = {
   signIn: "/authentication/sign-in",
   signPass: "/authentication/sign-in/signPass",
   signInfo: "/authentication/sign-in/signInfo",
-};
-=======
-    Home : '/',
-    Artist : '/artist',
-    Albums: '/albums',
-    AlbumSongs : '/albums/:id',
-    ArtistSongs : '/artist/:id',
-    Song : '/song/:id',
-    FullSong    :   '/songs',
-    signIn :"/sign-in",
-    signUp :"/sign-Up",
-    notFound :"*"
+  notFound :"*"
 
 }
->>>>>>> 19daeb7f8efbb5139b3d18380965ca4240b81bcb
+
 
 export default routes;
--- music_app/src/routes/routes.js
+++ music_app/src/routes/routes.js
@@ -1,38 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import React, { Component } from "react";
-import config from "../config";
 
-<<<<<<< HEAD
+
+import config from "../config";
 const Home = React.lazy(() => import("../pages/User/Home/Home"));
 const HomeIndex = React.lazy(() => import("../components/DisplayHome"));
 const FullAlbum = React.lazy(() => import("../components/FullAlbums"));
 const FullArtists = React.lazy(() => import("../components/FullArtists"));
-const FullSongPopular = React.lazy(() =>
-  import("../components/FullSongPopular")
-);
+const FullSongPopular = React.lazy(() => import("../components/FullSongPopular"));
 const AlbumSongs = React.lazy(() => import("../components/DisplayAlbum"));
 const ArtistSongs = React.lazy(() => import("../components/DisplayArtist"));
 const Song = React.lazy(() => import("../components/DetailSong"));
 const Login = React.lazy(() => import("../pages/User/Authentication/Login"));
-const LoginIndex = React.lazy(() =>
-  import("../components/authentication/DisplayLogin")
-);
-const ForgetPass = React.lazy(() =>
-  import("../components/authentication/ForgetPassword")
-);
-const ResetPass = React.lazy(() =>
-  import("../components/authentication/ResetPassword")
-);
+const LoginIndex = React.lazy(() => import("../components/authentication/DisplayLogin"));
+const ForgetPass = React.lazy(() => import("../components/authentication/ForgetPassword"));
+const ResetPass = React.lazy(() => import("../components/authentication/ResetPassword"));
 const Signin = React.lazy(() => import("../pages/User/Authentication/Signin"));
-const SigninIndex = React.lazy(() =>
-  import("../components/authentication/SignName")
-);
-const SignPass = React.lazy(() =>
-  import("../components/authentication/SignPassword")
-);
-const SignInfo = React.lazy(() =>
-  import("../components/authentication/SignInfo")
-);
+const SigninIndex = React.lazy(() => import("../components/authentication/SignName"));
+const SignPass = React.lazy(() => import("../components/authentication/SignPassword"));
+const SignInfo = React.lazy(() => import("../components/authentication/SignInfo"));
+const NotFound = React.lazy(() => import("../pages/NotFound"));
+
 
 const routes = createBrowserRouter([
   {
@@ -69,61 +57,10 @@ const routes = createBrowserRouter([
       },
     ],
   },
-=======
-const Home = React.lazy(() => import("../pages/User/Home/Home"))
-const HomeIndex = React.lazy(()=> import("../components/DisplayHome"))
-const FullAlbum = React.lazy(()=> import("../components/FullAlbums"))
-const FullArtists = React.lazy(()=>import("../components/FullArtists"))
-const FullSongPopular = React.lazy(()=>import("../components/FullSongPopular"))
-const AlbumSongs = React.lazy(()=>import("../components/DisplayAlbum"))
-const ArtistSongs = React.lazy(()=>import("../components/DisplayArtist"))
-const Song = React.lazy(()=>import("../components/DetailSong"))
-const NotFound = React.lazy(()=> import("../pages/NotFound"))
-
-const routes = createBrowserRouter([
-    {
-        path: config.routes.Home,
-        Component : Home,
-        children : [
-            {
-                index : true,
-                Component : HomeIndex
-            },
-            {
-                path : config.routes.Albums,
-                Component : FullAlbum
-            },
-            {
-                path : config.routes.FullSong,
-                Component : FullSongPopular
-            },
-            {
-                path : config.routes.Artist,
-                Component : FullArtists
-            },
-            {
-                path : config.routes.AlbumSongs,
-                Component : AlbumSongs
-            },
-            {
-                path : config.routes.ArtistSongs,
-                Component : ArtistSongs
-            },
-            {
-                path : config.routes.Song,
-                Component : Song
-            }
-            
-        ]
-    },
-    {
-        path : config.routes.notFound,
-        Component : NotFound
-    }
-   
-])
->>>>>>> 19daeb7f8efbb5139b3d18380965ca4240b81bcb
-
+  {
+    path: config.routes.notFound,
+    Component: NotFound,
+  },
   {
     path: config.routes.logIn,
     Component: Login,
@@ -148,7 +85,7 @@ const routes = createBrowserRouter([
     Component: Signin,
     children: [
       {
-        index: true, // Trang con mặc định
+        index: true,
         Component: SigninIndex,
       },
       {
