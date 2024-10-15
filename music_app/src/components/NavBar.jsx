1d86e4922fa5e2b0ab9540c201da460ba4b49d91
doanhdai <doanhdaigr5.2004@gmail.com>
2024-10-15 10:36:35 +0700

    nav info

--- music_app/src/components/NavBar.jsx
+++ music_app/src/components/NavBar.jsx
@@ -1,6 +1,10 @@
 import React, { useState, startTransition } from "react";
 import { IoIosSearch } from "react-icons/io";
 import { assets } from "../assets/assets";
+import { FaRegBell } from "react-icons/fa6";
+import { IoSettingsOutline } from "react-icons/io5";
+import { CiLogin } from "react-icons/ci";
+import { RiAccountCircleLine } from "react-icons/ri";
 import { Link, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
@@ -26,7 +30,7 @@ const NavBar = () => {
         </div>
 
 
-        <div className="flex gap-2 items-center justify-center mr-10">
+        <div className="flex gap-2 items-center justify-center">
           <Link to="/" className="m-0  no-underline bg-[#E0066F] p-3 rounded-full ">
             <img
               className="w-6 hover:scale-110"
@@ -45,6 +49,8 @@ const NavBar = () => {
         </div>
 
         <div className="flex items-center gap-4">
+
+          {/* Chưa đăng nhập, đăng kí */}
           {/* <p
             className="text-gray-400 text-[15px] px-5 p-3 rounded-3xl hidden md:block cursor-pointer hover:text-white hover:scale-110"
             onClick={() => {
@@ -69,13 +75,21 @@ const NavBar = () => {
           >
             Đăng nhập
           </p> */}
+          {/* hết chưa đăng nhập,đăng kí */}
 
+
+
+          {/* đã đăng nhập */}
+            <p className='bg-[#E0066F] text-while text-[15px] px-4 py-2 rounded-3xl hidden md:block cursor-pointer '>Khám phá Primeum</p>
+            <div className="cursor-pointer">
+              <FaRegBell size={25} />
+            </div>
            <div
               className="relative"
               onMouseEnter={() => setIsOpen(true)}
               
             >
-              {/* Thẻ P cho người dùng */}
+             
               <p className="bg-purple-500 text-black w-10 h-10 rounded-full flex items-center justify-center">
                 Đ
               </p>
@@ -84,19 +98,23 @@ const NavBar = () => {
               {isOpen && (
                 <div onMouseLeave={() => setIsOpen(false)} className="absolute top-12 right-0 bg-gray-800 shadow-lg rounded-lg py-2 px-3 w-48">
                   <ul className="text-white">
-                    <li className="hover:bg-black p-2 rounded-md cursor-pointer">
+                    <li className="hover:bg-black p-2 rounded-md cursor-pointer flex items-center ">
+                      <div className="mr-3"><IoSettingsOutline size={20}/></div>
                       Quản lý
                     </li>
-                    <li className="hover:bg-black p-2 rounded-md cursor-pointer">
+                    <li className="hover:bg-black p-2 rounded-md cursor-pointer flex items-center">
+                      <div className="mr-3"><RiAccountCircleLine size={20}/></div>
                       Tài khoản
                     </li>
-                    <li className="hover:bg-black p-2 rounded-md cursor-pointer">
+                    <li className="hover:bg-black p-2 rounded-md cursor-pointer flex items-center">
+                      <div className="mr-3"><CiLogin size={20}/></div>
                       Đăng xuất
                     </li>
                   </ul>
                 </div>
               )}
             </div>
+            {/* hết đã đăng nhâp */}
         </div>
       </div>
       
