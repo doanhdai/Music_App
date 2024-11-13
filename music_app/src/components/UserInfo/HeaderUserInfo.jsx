ddca05fa8673202c08d55055cfe29c0fa27e3d60
doanhdai <doanhdaigr5.2004@gmail.com>
2024-11-14 00:13:14 +0700

    filter song, album, account ben admin

--- music_app/src/components/UserInfo/HeaderUserInfo.jsx
+++ music_app/src/components/UserInfo/HeaderUserInfo.jsx
@@ -8,8 +8,8 @@ import { useNavigate } from "react-router-dom";
 import ComboIcon from "../Admin/ComboIcon/ComboIcon";
 
 const HeaderUserInfo = () => {
-  const navigate = useNavigate();
-  const [isOpen, setIsOpen] = useState(false);
+  // const navigate = useNavigate();
+  // const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="w-full flex justify-between items-center font-semibold">
       <p className="text-[25px] font-bold text-pink-500 sm:text-[20px]">
