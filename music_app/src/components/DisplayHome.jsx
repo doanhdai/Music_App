6747bac49c4312b430727d7b3181b4c453a5901c
angsongji <hoichima33@gmail.com>
2024-11-22 19:56:49 +0700

    loader home va loader playist

--- music_app/src/components/DisplayHome.jsx
+++ music_app/src/components/DisplayHome.jsx
@@ -13,104 +13,110 @@ const DisplayHome = () => {
   // console.log(songsData)
   return (
     <>
+      {(songsData.length != 0 && albumsData.length != 0 && artistsData.length != 0) ? (
+        <>
+          <div>
+            <div className="flex items-center justify-between">
+              {artistsData.length > 0 ? (
+                <h1 className="mb-4 font-bold text-2xl">Nghệ sĩ đề xuất</h1>
+              ) : (
+                ""
+              )}
+              {artistsData.length > 6 ? (
+                <h1
+                  className="text-slate-200 mr-3 cursor-pointer hover:text-white"
+                  onClick={() => navigate(`/artist`)}
+                >
+                  {" "}
+                  Xem tất cả
+                </h1>
+              ) : (
+                ""
+              )}
+            </div>
+            {/* <h1 className='my-4 font-bold'>Nghệ sĩ đề xuất</h1> */}
+            <div className="flex overflow-auto">
+              {artistsData.slice(0, 6).map((item, index) => (
+                <ArtistItems
+                  key={index}
+                  name={item.ten_artist}
+                  id={item.ma_artist}
+                  img={assets.mck}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="mb-4 pt-10">
+            <div className="flex items-center justify-between">
+              {albumsData.length > 0 ? (
+                <h1 className="mb-4 font-bold text-2xl">Album phổ biến</h1>
+              ) : (
+                ""
+              )}
+              {albumsData.length > 6 ? (
+                <h1
+                  className="text-slate-200 mr-3 cursor-pointer hover:text-white"
+                  onClick={() => navigate(`/albums`)}
+                >
+                  Xem tất cả
+                </h1>
+              ) : (
+                ""
+              )}
+            </div>
+            <div className="flex overflow-auto justify-start">
+              {albumsData.slice(0, 6).map((item, index) => (
+                <AlbumItems
+                  key={index}
+                  name={item.ten_album}
+                  desc={item.nguoi_so_huu}
+                  id={item.ma_album}
+                  img={item.hinh_anh}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="mb-4 pt-10">
+            <div className="flex items-center justify-between">
+              {songsData.length > 0 ? (
+                <h1 className="mb-4 font-bold text-2xl">Bài hát thịnh hành</h1>
+              ) : (
+                ""
+              )}
+              {songsData.length > 6 ? (
+                <h1
+                  className="text-slate-200 mr-3 cursor-pointer hover:text-white"
+                  onClick={() => navigate(`/songs`)}
+                >
+                  {" "}
+                  Xem tất cả
+                </h1>
+              ) : (
+                ""
+              )}
+            </div>
+            <div className="flex overflow-auto justify-start">
+              {songsData
+                .filter((item) => item.chat_luong === "Thấp")
+                .slice(0, 6)
+                .map((item, index) => (
+                  <SongItems
+                    key={index}
+                    name={item.ten_bai_hat}
+                    artistName={item.artist}
+                    id={item.ma_bai_hat}
+                    img={item.hinh_anh}
+                  />
+                ))}
+            </div>
+          </div>
+        </>
+      ) : <div className="wrap-loader"><span className="loader"></span></div>
+      }
       {/* nghệ sĩ */}
-      <div>
-        <div className="flex items-center justify-between">
-          {artistsData.length > 0 ? (
-            <h1 className="mb-4 font-bold text-2xl">Nghệ sĩ đề xuất</h1>
-          ) : (
-            ""
-          )}
-          {artistsData.length > 6 ? (
-            <h1
-              className="text-slate-200 mr-3 cursor-pointer hover:text-white"
-              onClick={() => navigate(`/artist`)}
-            >
-              {" "}
-              Xem tất cả
-            </h1>
-          ) : (
-            ""
-          )}
-        </div>
-        {/* <h1 className='my-4 font-bold'>Nghệ sĩ đề xuất</h1> */}
-        <div className="flex overflow-auto">
-          {artistsData.slice(0, 6).map((item, index) => (
-            <ArtistItems
-              key={index}
-              name={item.ten_artist}
-              id={item.ma_artist}
-              img={assets.mck}
-            />
-          ))}
-        </div>
-      </div>
 
-      <div className="mb-4 pt-10">
-        <div className="flex items-center justify-between">
-          {albumsData.length > 0 ? (
-            <h1 className="mb-4 font-bold text-2xl">Album phổ biến</h1>
-          ) : (
-            ""
-          )}
-          {albumsData.length > 6 ? (
-            <h1
-              className="text-slate-200 mr-3 cursor-pointer hover:text-white"
-              onClick={() => navigate(`/albums`)}
-            >
-              Xem tất cả
-            </h1>
-          ) : (
-            ""
-          )}
-        </div>
-        <div className="flex overflow-auto justify-start">
-          {albumsData.slice(0, 6).map((item, index) => (
-            <AlbumItems
-              key={index}
-              name={item.ten_album}
-              desc={item.nguoi_so_huu}
-              id={item.ma_album}
-              img={item.hinh_anh}
-            />
-          ))}
-        </div>
-      </div>
-
-      <div className="mb-4 pt-10">
-        <div className="flex items-center justify-between">
-          {songsData.length > 0 ? (
-            <h1 className="mb-4 font-bold text-2xl">Bài hát thịnh hành</h1>
-          ) : (
-            ""
-          )}
-          {songsData.length > 6 ? (
-            <h1
-              className="text-slate-200 mr-3 cursor-pointer hover:text-white"
-              onClick={() => navigate(`/songs`)}
-            >
-              {" "}
-              Xem tất cả
-            </h1>
-          ) : (
-            ""
-          )}
-        </div>
-        <div className="flex overflow-auto justify-start">
-          {songsData
-            .filter((item) => item.chat_luong === "Thấp")
-            .slice(0, 6)
-            .map((item, index) => (
-              <SongItems
-                key={index}
-                name={item.ten_bai_hat}
-                artistName={item.artist}
-                id={item.ma_bai_hat}
-                img={item.hinh_anh}
-              />
-            ))}
-        </div>
-      </div>
     </>
   );
 };
--- music_app/src/components/DisplayPlaylist.jsx
+++ music_app/src/components/DisplayPlaylist.jsx
@@ -22,7 +22,7 @@ const DisplayPlaylist = () => {
 
   const url_api = "http://localhost:8000";
 
-  
+
   const getSongByAlbumsData = async () => {
     try {
       const response = await axios.get(
@@ -47,109 +47,112 @@ const DisplayPlaylist = () => {
   const closeMenu = () => setMenuSongId(null);
 
   return (
-    <div onClick={closeMenu}>
-      <div className="mt-10 flex gap-8 flex-col md:flex-row md:items-col">
-        <img className="w-48 rounded" src={assets.mck} alt="Playlist cover" />
-        <div className="flex flex-col justify-center">
-          <p>Playlist</p>
-          <h2 className="text-5xl font-bold mb-4 md:text-7xl">
-            {detailPlaylist.ten_playlist}
-          </h2>
-          <p className="mt-1 flex items-center">
-            <img className="w-5" src={assets.spotify_logo} alt="Spotify logo" />
-            <b className="pl-2">Đài -</b>
-            <b className="pl-2">{songsPlaylist.length} bài hát</b>
-          </p>
-        </div>
-      </div>
+    <>
+      {
+        (detailPlaylist.length != 0 && songsPlaylist.length != 0) ? (
+          <div onClick={closeMenu}>
+            <div className="mt-10 flex gap-8 flex-col md:flex-row md:items-col">
+              <img className="w-48 rounded" src={assets.mck} alt="Playlist cover" />
+              <div className="flex flex-col justify-center">
+                <p>Playlist</p>
+                <h2 className="text-5xl font-bold mb-4 md:text-7xl">
+                  {detailPlaylist.ten_playlist}
+                </h2>
+                <p className="mt-1 flex items-center">
+                  <img className="w-5" src={assets.spotify_logo} alt="Spotify logo" />
+                  <b className="pl-2">Đài -</b>
+                  <b className="pl-2">{songsPlaylist.length} bài hát</b>
+                </p>
+              </div>
+            </div>
 
-      <div>
-        <div className="mt-10">
-          <div className="flex gap-10 items-center">
-            <button className="w-[60px] h-[60px] rounded-full bg-[#E0066F] flex justify-center items-center">
-              {playStatus ? (
-                <IoMdPause onClick={pause} size={20} />
-              ) : (
-                <FaPlay onClick={() => playWithId(songsArtist.ma_bai_hat)} />
-              )}
-            </button>
-          </div>
-        </div>
-        <h1 className="font-bold text-2xl mt-7 mb-5">Danh sách phát</h1>
-        <div className="grid grid-cols-5 sm:grid-cols-[0.2fr_2.8fr_2fr_0.5fr] mb-4 pl-2 text-[#fff]">
-          <b className="mr-4">#</b>
-          <p> Title</p>
-          <p>Album</p>
-          {/* <img
+            <div>
+              <div className="mt-10">
+                <div className="flex gap-10 items-center">
+                  <button className="w-[60px] h-[60px] rounded-full bg-[#E0066F] flex justify-center items-center">
+                    {playStatus ? (
+                      <IoMdPause onClick={pause} size={20} />
+                    ) : (
+                      <FaPlay onClick={() => playWithId(songsArtist.ma_bai_hat)} />
+                    )}
+                  </button>
+                </div>
+              </div>
+              <h1 className="font-bold text-2xl mt-7 mb-5">Danh sách phát</h1>
+              <div className="grid grid-cols-5 sm:grid-cols-[0.2fr_2.8fr_2fr_0.5fr] mb-4 pl-2 text-[#fff]">
+                <b className="mr-4">#</b>
+                <p> Title</p>
+                <p>Album</p>
+                {/* <img
             className="m-auto w-4"
             src={assets.clock_icon}
             alt="Clock icon"
           /> */}
-          <p className="flex justify-center">Thêm</p>
-        </div>
+                <p className="flex justify-center">Thêm</p>
+              </div>
 
-        <hr />
+              <hr />
 
-        {songsPlaylist.map((item, index) => (
-          <div
-            key={index}
-            className="grid grid-cols-5 sm:grid-cols-[0.2fr_2.8fr_2fr_0.5fr] mt-10 mb-4 pl-2 text-[#fff] items-center hover:bg-[#ffffff2b] cursor-pointer"
-            onMouseEnter={() => setHoveredSong(index)}
-            onMouseLeave={() => setHoveredSong(null)}
-          >
-            {playStatus && track.ma_bai_hat === item.ma_bai_hat ? (
-              <p>
-                {hoveredSong === index ? (
-                  <IoMdPause onClick={pause} size={13} />
-                ) : (
-                  index + 1
-                )}
-              </p>
-            ) : (
-              <p>
-                {hoveredSong === index ? (
-                  <FaPlay
-                    onClick={() => playWithId(item.ma_bai_hat)}
-                    size={13}
-                  />
-                ) : (
-                  index + 1
-                )}
-              </p>
-            )}
-            <Link to={`/song/${item.ma_bai_hat}`} className="text-white">
-              <img
-                className="inline w-10 mr-4"
-                src={assets.mck}
-                alt="Song cover"
-              />
-              {item.ten_bai_hat}
-            </Link>
-            <p className="text-[15px]">{item.album}</p>
-            <div className="text-[15px] flex justify-center relative">
-              {menuSongId === item.ma_bai_hat && (
-                <div className="absolute bottom-8 right-0 bg-gray-800 text-white p-2 rounded shadow-lg !z-50 w-[250px]">
-                  <div className="hover:bg-black p-2 cursor-pointer flex items-center gap-2">
-                    <IoAddCircleOutline size={20} />
-                    Xóa khỏi playlist
-                  </div>
-                  <div className="hover:bg-black p-2 cursor-pointer flex items-center gap-2">
-                    <MdArrowCircleDown size={20} />
-                    Tải xuống
+              {songsPlaylist.map((item, index) => (
+                <div
+                  key={index}
+                  className="grid grid-cols-5 sm:grid-cols-[0.2fr_2.8fr_2fr_0.5fr] mt-10 mb-4 pl-2 text-[#fff] items-center hover:bg-[#ffffff2b] cursor-pointer"
+                  onMouseEnter={() => setHoveredSong(index)}
+                  onMouseLeave={() => setHoveredSong(null)}
+                >
+                  {playStatus && track.ma_bai_hat === item.ma_bai_hat ? (
+                    <p>
+                      {hoveredSong === index ? (
+                        <IoMdPause onClick={pause} size={13} />
+                      ) : (
+                        index + 1
+                      )}
+                    </p>
+                  ) : (
+                    <p>
+                      {hoveredSong === index ? (
+                        <FaPlay
+                          onClick={() => playWithId(item.ma_bai_hat)}
+                          size={13}
+                        />
+                      ) : (
+                        index + 1
+                      )}
+                    </p>
+                  )}
+                  <Link to={`/song/${item.ma_bai_hat}`} className="text-white">
+                    <img
+                      className="inline w-10 mr-4"
+                      src={assets.mck}
+                      alt="Song cover"
+                    />
+                    {item.ten_bai_hat}
+                  </Link>
+                  <p className="text-[15px]">{item.album}</p>
+                  <div className="text-[15px] flex justify-center relative">
+                    {menuSongId === item.ma_bai_hat && (
+                      <div className="absolute bottom-8 right-0 bg-gray-800 text-white p-2 rounded shadow-lg !z-50 w-[250px]">
+                        <div className="hover:bg-black p-2 cursor-pointer flex items-center gap-2">
+                          <IoAddCircleOutline size={20} />
+                          Xóa khỏi playlist
+                        </div>
+                        <div className="hover:bg-black p-2 cursor-pointer flex items-center gap-2">
+                          <MdArrowCircleDown size={20} />
+                          Tải xuống
+                        </div>
+                      </div>
+                    )}
+                    <IoIosMore
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        toggleMenu(item.ma_bai_hat);
+                      }}
+                    />
                   </div>
                 </div>
-              )}
-              <IoIosMore
-                onClick={(e) => {
-                  e.stopPropagation();
-                  toggleMenu(item.ma_bai_hat);
-                }}
-              />
+              ))}
             </div>
-          </div>
-        ))}
-      </div>
-      {/* <div className="mb-4 pt-10">
+            {/* <div className="mb-4 pt-10">
         <div className="flex justify-between">
           <h1 className="my-4 font-bold text-2xl">Danh sách album</h1>
           <h1
@@ -170,7 +173,11 @@ const DisplayPlaylist = () => {
           ))}
         </div>
       </div> */}
-    </div>
+          </div>
+        ) : <div className="wrap-loader"><span className="loader"></span></div>
+      }
+    </>
+
   );
 };
 
