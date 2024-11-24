07faaef8d5eca6a0d46e91831b6b7be9f451fc4b
Light <tranhongquang24@gmail.com>
2024-11-24 11:22:50 +0700

    Update detail song, wait for correct api

--- music_app/src/pages/artist/components/SongDetailModal.jsx
+++ music_app/src/pages/artist/components/SongDetailModal.jsx
@@ -1,9 +1,13 @@
-import {  FaHeadphones, FaHeart, IoTimeSharp } from "react-icons"
-import { songData2 } from "../../../assets/assets";
 
-const SongDetailModal = ({ song, onClose }) => {
-  if (!song) return null;
-  const songData = songData2[1];
+import { FaHeart } from "react-icons/fa";
+import { FaHeadphones } from "react-icons/fa6";
+import { IoTimeSharp } from "react-icons/io5";
+import { FaX } from "react-icons/fa6";
+import { extractDayMonthYear } from "../../../assets/assets";
+
+const SongDetailModal = ({ detailsSongModalState, songData, onClose }) => {
+  if (!detailsSongModalState) return null;
+  //console.log(songData)
   return (
     <div
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
@@ -13,51 +17,51 @@ const SongDetailModal = ({ song, onClose }) => {
         <div className="flex items-center gap-10 justify-center ">
           <div className="flex items-stretch">
             <img
-              // src={songData.hinh_anh}
-              src="https://cloudinary-marketing-res.cloudinary.com/image/upload/ar_0.5,c_fill,g_auto,w_433/q_auto/f_auto/hiking_dog_mountain.jpg"
+              src={songData.hinh_anh}
+              //src="https://cloudinary-marketing-res.cloudinary.com/image/upload/ar_0.5,c_fill,g_auto,w_433/q_auto/f_auto/hiking_dog_mountain.jpg"
               alt={songData.ten_bai_hat}
               className="mb-4 w-40 h-40 rounded"
             />
 
             <div className="flex flex-col justify-between ml-4 gap-5 text-white">
               <h5 className="text-sm text-gray-400">
-                {songData.trang_thai === 1 ? "Cong khai" : "An"}
+                {songData.trang_thai === 1 ? "Công khai" : "Ẩn"}
               </h5>
               <h3 className="text-lg text-wrap font-semibold">
                 {songData.ten_bai_hat}
               </h3>
               <h5 className="text-sm text-gray-400">
-                Ngay phat hanh:{" "}
-                <span className="text-white">{songData.ngay_phat_hanh}</span>
+                Ngày phát hành:{" "}
+                <span className="text-white">  {extractDayMonthYear(songData.ngay_phat_hanh)}</span>
               </h5>
               <h5 className="text-sm text-gray-400">
-                Album:{" "}
-                <span className="text-white">{songData.ma_album} ma album</span>
+                Album:
+                <span className="text-white">   {songData.ten_album? songData.ten_album : "Không có"}</span>
               </h5>
               <h5 className="text-sm text-gray-400">
-                The loai:{" "}
+                Thể loại:
                 <span className="text-white">
-                  99%{songData.theloa} the loai
+                  {songData.the_loai} 
                 </span>
               </h5>
               <h5 className="text-sm text-gray-400">
-                Nghe si:{" "}
+                Nghệ sĩ:
                 <span className="text-white">{songData.ma_artist}</span>
               </h5>
             </div>
           </div>
           <div className="flex flex-col  gap-3 justify-between w-[60] ml-2">
             <div className="flex flex-row items-center text-gray-400 gap-2">
-              <FaHeart class />
-              <span>11{song.luot_yeu_thich}</span>
+              <FaHeart className="" />
+              <span>{songData.luot_yeu_thich}</span>
             </div>
             <div className="flex flex-row items-center text-gray-400 gap-2">
               <FaHeadphones />
-              <span>11{song.luot_nghe}</span>
+              <span>{songData.luot_nghe}</span>
             </div>
             <div className="flex flex-row items-center text-gray-400 gap-2">
               <IoTimeSharp />
-              <span>3:2</span>
+              <span>{songData.thoi_luong}</span>
             </div>
           </div>
         </div>
@@ -66,8 +70,7 @@ const SongDetailModal = ({ song, onClose }) => {
           onClick={onClose}
           className="absolute top-2 right-2 text-white py-2 px-4 rounded"
         >
-          {" "}
-          X
+         <FaX/>
         </button>
       </div>
     </div>
