d5d84c68e09f25ef6c799763a3d7c4832fdb910a
voanhtuan697 <anhtuanvo697@gmail.com>
2024-12-02 09:50:41 +0700

    update acc

--- music_app/src/components/UserInfo/AccountProfile.jsx
+++ music_app/src/components/UserInfo/AccountProfile.jsx
@@ -2,24 +2,13 @@ import React, { useState } from "react";
 import InputItem from "./InputItem";
 import { FaPen } from "react-icons/fa";
 import { FiEye, FiEyeOff } from "react-icons/fi";
-const AccountProfile = ({onEdit,email,datetime,password}) => {
+const AccountProfile = ({onEdit,email,datetime}) => {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
   const togglePasswordVisibility = () => {
     setIsPasswordVisible(!isPasswordVisible);
   };
 
-const date = new Date();
-
-// Lấy các phần của ngày tháng năm
-const day = date.getDate().toString().padStart(2, '0');           // Thêm số 0 nếu ngày < 10
-const month = (date.getMonth() + 1).toString().padStart(2, '0');  // Tháng bắt đầu từ 0, cần cộng thêm 1 và thêm số 0 nếu tháng < 10
-const year = date.getFullYear();                                // Lấy năm đầy đủ
-
-// Tạo chuỗi định dạng dd/mm/yyyy
-const formattedDate = `${day}/${month}/${year}`;
-
-// console.log(formattedDate);  // Ví dụ: "14/11/2024"
   return (
     <div className="bg-[#141414] text-white rounded-lg p-6 w-96 relative">
       <h2 className="text-pink-500 font-bold mb-6">Thông tin cá nhân</h2>
@@ -30,16 +19,16 @@ const formattedDate = `${day}/${month}/${year}`;
         <div className="font-bold text-[#fff] text-[16px]">Email: </div>
         <div className="text-[#ccc] text-[16px] ml-2">{email}</div>
       </div>
-      <div className="flex mt-4">
+      {/* <div className="flex mt-4">
         <div className="font-bold text-[#fff] text-[16px]">Mật khẩu: </div>
         <input type={isPasswordVisible ? "text" : "password"} value={password} className="text-[#ccc] text-[16px] ml-2 bg-transparent focus:outline-none" readOnly/>
         <button type="button" onClick={togglePasswordVisibility} className="absolute top-[119px] right-14 flex items-center text-gray-500">{isPasswordVisible ? <FiEyeOff /> : <FiEye />}</button>
-      </div>
+      </div> */}
       <div className="flex mt-4">
         <div className="font-bold text-[#fff] text-[16px]">
           Ngày tạo tài khoản:{" "}
         </div>
-        <div className="text-[#ccc] text-[16px] ml-2">{formattedDate}</div>
+        <div className="text-[#ccc] text-[16px] ml-2">{datetime}</div>
       </div>
     </div>
   );
